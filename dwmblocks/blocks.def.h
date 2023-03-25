207d31f22b42c1d18b477cef19f28cf85c432f04
anderson.inacio.note <nosrednawall@gmail.com>
2023-03-25 12:10:26 -0300

    adicionado st term

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.dwm/dwmblocks/scripts/wifisignal",1 , 0},
+
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	
 	{"", "~/.dwm/dwmblocks/scripts/battery",	120,		0},
@@ -9,5 +11,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
